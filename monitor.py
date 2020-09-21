7ca8aed67180862ef2e4b4d14420e8e6abba5d22
Tony Jackson <tony.aaj.jackson@gmail.com>
2020-09-21 01:27:46 -0700

    Add pseudocode

---
+++ monitor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# Get messages from discord
+
+
+# For each message, check if contains spotify link
+    # If song
+        # Add to overall playlist
+        # Add to weekly playlist
+    # If album
+         # Add all songs on album to overall playlist
+         # Add all songs to weekly playlist
+
+
