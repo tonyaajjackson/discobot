c80db8c784fbe7d086d28a9e2ee3fae88595027e
Tony Jackson <tony.aaj.jackson@gmail.com>
2020-09-25 11:34:46 -0700

    Fix indentation errors

--- monitor.py
+++ monitor.py
@@ -57,26 +57,26 @@ try:
 
     @discord_client.event
     async def on_message(message):
-    if message.author == discord_client.user: return
-    if message.channel.name != DISCORD_CHANNEL: return
+        if message.author == discord_client.user: return
+        if message.channel.name != DISCORD_CHANNEL: return
 
-    if link :=spotify_link_regex.search(message.content):
-            link_type = link.group(1)
-            link_id = link.group(2)
+        if link :=spotify_link_regex.search(message.content):
+                link_type = link.group(1)
+                link_id = link.group(2)
 
-            if link_type == "track":
-                add_if_unique_tracks(SPOTIFY_ALL_TIME_PLAYLIST_ID, [link_id])
-                add_if_unique_tracks(SPOTIFY_WEEKLY_PLAYLIST_ID, [link_id])
+                if link_type == "track":
+                    add_if_unique_tracks(SPOTIFY_ALL_TIME_PLAYLIST_ID, [link_id])
+                    add_if_unique_tracks(SPOTIFY_WEEKLY_PLAYLIST_ID, [link_id])
 
-            if link_type == "album":
-            album_track_ids = [item['id'] for item in sp.album_tracks(link_id)['items']]
-                add_if_unique_tracks(SPOTIFY_ALL_TIME_PLAYLIST_ID, album_track_ids)
-                add_if_unique_tracks(SPOTIFY_WEEKLY_PLAYLIST_ID, album_track_ids)
+                if link_type == "album":
+                    album_track_ids = [item['id'] for item in sp.album_tracks(link_id)['items']]
+                    add_if_unique_tracks(SPOTIFY_ALL_TIME_PLAYLIST_ID, album_track_ids)
+                    add_if_unique_tracks(SPOTIFY_WEEKLY_PLAYLIST_ID, album_track_ids)
 
-            if link_type == "artist":
-            top_song_ids = [item['id'] for item in sp.artist_top_tracks(link_id)['tracks']]
-                add_if_unique_tracks(SPOTIFY_ALL_TIME_PLAYLIST_ID, top_song_ids)
-                add_if_unique_tracks(SPOTIFY_WEEKLY_PLAYLIST_ID, top_song_ids)
+                if link_type == "artist":
+                    top_song_ids = [item['id'] for item in sp.artist_top_tracks(link_id)['tracks']]
+                    add_if_unique_tracks(SPOTIFY_ALL_TIME_PLAYLIST_ID, top_song_ids)
+                    add_if_unique_tracks(SPOTIFY_WEEKLY_PLAYLIST_ID, top_song_ids)
 
 
     # Spotify functions
@@ -86,7 +86,7 @@ try:
 
         playlist = sp.playlist_items(playlist_id)
         playlist_track_ids = set(item['track']['id'] for item in playlist['items'])
-    unique_track_ids = set(id for id in track_ids if id not in playlist_track_ids)
+        unique_track_ids = set(id for id in track_ids if id not in playlist_track_ids)
         if unique_track_ids:
             sp.playlist_add_items(playlist_id, list(unique_track_ids))
 
