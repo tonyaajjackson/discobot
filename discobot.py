6de1998d65c1d2b8ba21a4c1c4c3619ff806c41d
Tony Jackson <tony.aaj.jackson@gmail.com>
2020-12-05 10:10:25 -0800

    Handle >100 track playlist in uniqueness check
    sp.playlist_items can return a maximum of 100 tracks, so for playlists
    with >100 tracks, the uniqueness check would only consider the first 100
    tracks.

--- discobot.py
+++ discobot.py
@@ -122,12 +122,18 @@ def add_if_unique_tracks(playlist_uri, track_ids):
     assert type(track_ids) == list
 
     try:
-        playlist = sp.playlist_items(playlist_uri)
+        playlist_tracks = []
+        offset = 0
+
+        while playlist_temp := sp.playlist_items(playlist_uri, offset=offset)['items']:
+            playlist_tracks += playlist_temp
+            offset += 100
+        
     except spotipy.exceptions.SpotifyException:
         logging.exception("Error in getting tracks from playlist ID: " + playlist_uri, exc_info=True)
         return
 
-    playlist_track_ids = set(item['track']['id'] for item in playlist['items'])
+    playlist_track_ids = set(item['track']['id'] for item in playlist_tracks)
     unique_track_ids = set(id for id in track_ids if id not in playlist_track_ids)
     if unique_track_ids:
         try:
