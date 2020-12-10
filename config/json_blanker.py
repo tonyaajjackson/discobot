c6711d7cb4620d7df721af7a9931337aa87c7a67
Tony Jackson <tony.aaj.jackson@gmail.com>
2020-12-09 22:07:57 -0800

    Add json_blanker to generate template json files
    Previously I had the template json files alongside populated json files
    and would make changes to the populated json files during development.
    When the time came to commit, I would have to manually update the
    templates and would sometimes forget. With the json blanker, I can now
    just blank the populated json files to make up-to-date templates.

---
+++ config/json_blanker.py
@@ -0,0 +1,38 @@
+import json
+import sys
+
+
+def blank_json(input_object):
+    if type(input_object) is list:
+        output_list = []
+        for item in input_object:
+            output_list.append(blank_json(item))
+
+        return output_list
+    
+    elif type(input_object) is dict:
+        output_dict = {}
+        for (key, val) in input_object.items():
+            output_dict[key] = blank_json(val)
+        
+        return output_dict
+
+    else:
+        if type(input_object) is str:
+            return ""
+        elif type(input_object) is int:
+            return 0
+        elif type(input_object) is bool:
+            return False
+        elif type(input_object) is None:
+            return None
+
+
+for arg in sys.argv[1:]:
+    with open("./config/" + arg, "r") as f:
+        raw = json.load(f)
+    
+    blanked = blank_json(raw)
+    
+    with open("./config/template_" + arg, "w") as f:
+        json.dump(blanked, f, indent=4)
