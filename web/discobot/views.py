abc5ae359fbd62c0249198644d28562f611e9435
Tony Jackson <tony.aaj.jackson@gmail.com>
2021-03-04 19:59:39 -0800

    Add linking to discord OAuth and getting token

--- web/discobot/views.py
+++ web/discobot/views.py
@@ -1,6 +1,18 @@
+import os
+from urllib.parse import urlencode
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
+DISCORD_OAUTH_URL = "https://discord.com/api/oauth2/authorize?"
+discord_params = {
+    "client_id": os.environ['DISCORD_CLIENT_ID'],
+    "redirect_uri": os.environ['DISCORD_REDIRECT_URI'],
+    "response_type": "code",
+    "scope": "identify"
+}
+
+login_url = DISCORD_OAUTH_URL + urlencode(discord_params)
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    return render(request, "discobot/login.html", {"login_url": login_url})
