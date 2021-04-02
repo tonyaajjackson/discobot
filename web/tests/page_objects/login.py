06398f89fff2f49c6053a3b286178b91e428ae73
Tony Jackson <tony.aaj.jackson@gmail.com>
2021-04-01 18:32:01 -0700

    Delete old tests and page objects

--- web/tests/page_objects/login.py
+++
@@ -1,7 +0,0 @@
-class LoginPage(object):
-    def __init__(self, driver):
-        self.driver = driver
-        self.url = ""
-
-    def find_login_with_discord_link(self):
-        return self.driver.find_element_by_id("login-with-discord-link")
